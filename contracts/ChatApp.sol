6d858937cd3fe2b7c4d0d169f04156be19266356
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-30 05:08:04 +0530

    feat: Added bunch of function for creating an account

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -19,4 +19,21 @@ contract ChatApp {
 
     mapping(address => user) userList;
     mapping(bytes32 => message[]) allMessages;
+
+    function checkUserExists(address pubkey) public view returns (bool) {
+        return bytes(userList[pubkey].name).length > 0;
+    }
+
+    // create_account:
+    function createAccount(string calldata name) external {
+        require(!checkUserExists(msg.sender), "User already exists");
+        require(bytes(name).length > 0, "Name cannot be empty");
+        userList[msg.sender].name = name;
+    }
+
+    // get_username:
+    function getUsername(address pubkey) external view returns (string memory) {
+        require(checkUserExists(pubkey), "User does not exist");
+        return userList[pubkey].name;
+    }
 }
