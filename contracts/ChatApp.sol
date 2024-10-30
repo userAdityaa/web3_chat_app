98df906eb2c0987464ea3b07bdab86939f3c9c2f
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-30 05:46:30 +0530

    feat: Logic for pushing friend, checking friend

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract ChatApp {
     struct user {
         string name;
-        friend[] friends;
+        friend[] friendList;
     }
 
     struct friend {
@@ -35,5 +35,49 @@ contract ChatApp {
     function getUsername(address pubkey) external view returns (string memory) {
         require(checkUserExists(pubkey), "User does not exist");
         return userList[pubkey].name;
+    }
+
+    // add_friend:
+    function addFriend(address friend_key, string calldata name) external {
+        require(checkUserExists(msg.sender), "Create an account first");
+        require(checkUserExists(friend_key), "User does not exist");
+        require(msg.sender != friend_key, "Cannot add yourself as a friend");
+        require(
+            checkAlreadyFriends(msg.sender, friend_key) == false,
+            "Already friends"
+        );
+        _addFriend(msg.sender, friend_key, name);
+        _addFriend(friend_key, msg.sender, userList[msg.sender].name);
+    }
+
+    // check_already_friends:
+    function checkAlreadyFriends(
+        address pubkey1,
+        address pubkey2
+    ) internal view returns (bool) {
+        if (
+            userList[pubkey1].friendList.length >
+            userList[pubkey2].friendList.length
+        ) {
+            address temp = pubkey1;
+            pubkey1 = pubkey2;
+            pubkey2 = temp;
+        }
+
+        for (uint i = 0; i < userList[pubkey1].friendList.length; i++) {
+            if (userList[pubkey1].friendList[i].pubkey == pubkey2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function _addFriend(
+        address currentUser,
+        address friend_key,
+        string memory name
+    ) internal {
+        friend memory newFriend = friend(friend_key, name);
+        userList[currentUser].friendList.push(newFriend);
     }
 }
