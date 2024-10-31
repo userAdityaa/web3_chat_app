91b68de15f75ad397544443faa15d2c90d385c63
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-31 22:29:37 +0530

    update_context: Updated the context file for agreement between .js and .ts

--- context/ChatAppContext.js
+++ context/ChatAppContext.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { CheckIfWalletConnected, connectWallet, connectingWithContract } from '../utils/apiFeature'
+import React, { createContext, useContext } from 'react';
 
-export const ChatAppContext = React.createContext(); 
- 
-export const ChatAppProvider = ({children}) => { 
-    const title = "Hey welcome to blockchain chat app"; 
-    return (
-        <ChatAppContext.Provider value={{title}}>
-            {children}
-        </ChatAppContext.Provider>
-    )
-}
+export const ChatAppContext = createContext();
+
+export const ChatAppProvider = ({ children }) => {
+  const title = "Hey, welcome to blockchain chat app";
+  
+  return (
+    <ChatAppContext.Provider value={{ title }}>
+      {children}
+    </ChatAppContext.Provider>
+  );
+};
+
+export const useChatAppContext = () => {
+  const context = useContext(ChatAppContext);
+  if (!context) {
+    throw new Error("useChatAppContext must be used within a ChatAppProvider");
+  }
+  return context;
+};
