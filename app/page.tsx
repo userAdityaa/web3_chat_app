f36eeb53f828ba9af8480a63444ad09d3a05664e
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-31 22:56:37 +0530

    add_page: Added page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+import { AppProps } from 'next/app';
 import {ChatAppProvider} from '../context/ChatAppContext';
-import {Navbar} from './components/index'
+import {Navbar} from './components/index';
 
-export default function Home() {
+export default function Home({Component, pageProps}: AppProps) {
   return (
     <ChatAppProvider>
       <Navbar/>
