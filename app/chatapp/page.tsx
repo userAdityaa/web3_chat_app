143203b4753e4f3cbde051cf92403d26d696f431
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-11 02:37:00 +0530

    configure_chatapp: Configured the chat app file for provider information

--- app/chatapp/page.tsx
+++ app/chatapp/page.tsx
@@ -3,10 +3,26 @@ import React, { useContext } from 'react'
 import { ChatAppContext } from '@/context/ChatAppContext'
 
 const ChatApp = () => {
-    const {title} = useContext(ChatAppContext)
+  const { account, userName, friendList, friendMsg, loading, error } = useContext(ChatAppContext);
 
   return (
-    <div>{title}</div>
+    <div>
+      <h1>Hey, {userName || 'Guest'}</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p>Account: {account}</p>
+          <p>Friends:</p>
+          <ul>
+            {friendList.map((friend: any, index: any) => (
+              <li key={index}>{friend}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </div>
   )
 }
 
