af8c45ebf9bed93af4eba196097527a5ac3766f0
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-16 22:44:44 +0530

    module and css

--- app/components/Model/model.tsx
+++ app/components/Model/model.tsx
@@ -2,8 +2,13 @@ import Images from '../../assets';
 import { ChatAppContext } from '@/context/ChatAppContext';
 // import {Loader} from '../index';
 import { useContext, useState } from 'react';
-import Style from 'Model.module.css';
+import Style from './Module.module.css';
 import Image, { StaticImageData } from 'next/image';
+
+interface FunctionParams {
+  name: string;
+  accountAddress: string;
+}
 
 interface ModelProps {
   openModal: (value: boolean) => void;
@@ -11,8 +16,8 @@ interface ModelProps {
   head: string;
   info: string;
   smallInfo: string;
-  image: StaticImageData, 
-  functionName: () => void;
+  image: StaticImageData;
+  functionName: (params: FunctionParams) => void;
   address?: string;
 }
 
@@ -41,6 +46,22 @@ export const Model: React.FC<ModelProps> = ({openModal, title, head, info, small
             <div className={Style.Model_box_right_name_info}>
               <Image src = {Images.username} alt='user' width={30} height={30}></Image>
               <input type="text" placeholder={address || "Enter address"} onChange={(e) => setName(e.target.value)} />
+            </div>
+
+            <div className= {Style.Model_box_right_name_btn}>
+              <button onClick={() => functionName({name, accountAddress})}>
+                {""}
+                <Image src={Images.send} alt='send' width={30} height={30}/>
+                {""}
+                Submit
+              </button>
+
+              <button onClick={() => openModal(false)}>
+                {""}
+                <Image src={Images.send} alt='send' width={30} height={30}/>
+                {""}
+                Cancel
+              </button>
             </div>
           </div>
         </div>
